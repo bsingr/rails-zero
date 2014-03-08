012d543209430624d79aed124f7c31ca85447892
Jens Bissinger <mail@jens-bissinger.de>
2014-03-08 16:50:49 +0100

    Add Relais.pages_config and .configure_pages(&block).

--- lib/relais.rb
+++ lib/relais.rb
@@ -1,4 +1,12 @@
 require "relais/engine"
+require "relais/pages_config"
 
 module Relais
+  def self.configure_pages
+    yield pages_config
+  end
+
+  def self.pages_config
+    @config ||= PagesConfig.new
+  end
 end
