dd5721eee20baa2921154b708fd28c851509d0e1
Jens Bissinger <mail@jens-bissinger.de>
2014-03-09 00:21:38 +0100

    Add new config #paths_to_except_from_cleanup with default files for public dir.

--- lib/relais/pages_config.rb
+++ lib/relais/pages_config.rb
@@ -3,5 +3,13 @@ module Relais
     def links
       @links ||= []
     end
+
+    def paths_to_except_from_cleanup
+      @paths_to_except_from_cleanup ||= begin
+        %w[ 404.html 422.html 500.html favicon.ico ].map do |f|
+          Rails.root.join('public', f).to_s
+        end
+      end
+    end
   end
 end
