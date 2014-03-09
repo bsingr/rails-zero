7c36002f550f4b3238edd1c6ef2172156392d77d
Jens Bissinger <mail@jens-bissinger.de>
2014-03-09 13:00:41 +0100

    Fix capybara in generate site job.

--- app/jobs/rails_zero/generate_site_job.rb
+++ app/jobs/rails_zero/generate_site_job.rb
@@ -2,8 +2,9 @@ module RailsZero
   class GenerateSiteJob
     def run
       RailsZero.pages_config.links.each do |path|
-        Capybara.visit path
+        ::Capybara.visit path
       end
+      ::Capybara.reset_sessions!
     end
   end
 end
